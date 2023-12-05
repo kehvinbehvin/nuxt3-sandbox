0815b07b4db375be3cc9215b889f68cd034b1ff3
Kevin Ho <kevinho@redairship.com>
2023-12-05 19:10:42 +0800

    feat(youtube): add youtube player

---
+++ pages/youtube.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <YouTube
+        src="https://www.youtube.com/watch?v=jNQXAC9IVRw"
+        @ready="onReady"
+        ref="youtube" />
+  </div>
+</template>
+
+<script>
+import YouTube from "vue3-youtube";
+
+export default {
+ components: {
+   YouTube,
+ },
+  methods: {
+    onReady() {
+      this.$refs.youtube.playVideo()
+    }
+  },
+  data() {
+   return {
+     source: "https://www.youtube.com/watch?v=jNQXAC9IVRw"
+   }
+  }
+}
+</script>
