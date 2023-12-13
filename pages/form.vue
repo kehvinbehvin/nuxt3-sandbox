80ecb9556f2ebf4e8f26569458962eb110d4b0fd
Kevin Ho <kevinho@redairship.com>
2023-12-13 15:53:38 +0800

    feat(vuelidate): add vuelidate plugin

---
+++ pages/form.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    "First Name"<input type="text" v-model="this.firstName">
+  </div>
+  <div>
+    "Last Name"<input type="text" v-model="this.lastName">
+  </div>
+  <div>
+    "Email"<input type="text" v-model="this.contact.email">
+  </div>
+</template>
+<script>
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+
+export default {
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      contact: {
+        email: ''
+      }
+    }
+  },
+  validations () {
+    return {
+      firstName: { required },
+      lastName: { required },
+      contact: {
+        email: { required, email }
+      }
+    }
+  }
+}
+</script>
