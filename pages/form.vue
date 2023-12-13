a42a6be669b2a9e1ac471580fcf2edc93e12af49
Kevin Ho <kevinho@redairship.com>
2023-12-13 18:04:16 +0800

    feat(vuelidat): implement vuelidate

--- pages/form.vue
+++ pages/form.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    "First Name"<input type="text" v-model="this.firstName">
+  <div ref="firstName">
+    First Name<input type="text" v-model="this.v$.firstName.$model" @input="logger" @blur="this.v$.firstName.$touch">
+    {{this.firstName}}
+    <p>This field is dirty {{ this.v$.firstName.$anyDirty }}</p>
+    <p>This field is dirty {{ this.v$.firstName.$dirty }}</p>
+    <p>This field should be invalid invalid {{ this.v$.firstName.$invalid }}</p>
+    <p>This field should only show error after dirty and still invalid {{ this.v$.firstName.$error }}</p>
   </div>
-  <div>
-    "Last Name"<input type="text" v-model="this.lastName">
+  <div ref="lastName">
+    Last Name<input type="text" v-model="this.lastName" @blur="this.v$.lastName.$touch">
+    {{this.lastName}}
+    <p>This field is dirty {{ this.v$.lastName.$anyDirty }}</p>
   </div>
-  <div>
-    "Email"<input type="text" v-model="this.contact.email">
+  <div ref="email">
+    Email<input type="text" v-model="this.contact.email" @blur="this.v$.contact.email.$touch">
+    {{this.contact.email}}
+    <p>This field is dirty {{ this.v$.contact.email.$anyDirty }}</p>
+  </div>
+  <div @click="submit">
+    Submit
   </div>
 </template>
 <script>
@@ -33,6 +45,15 @@ export default {
       contact: {
         email: { required, email }
       }
+    }
+  },
+  methods: {
+    logger() {
+      console.log("V$: ", this.v$.firstName)
+      console.log("Refs: ", this.$refs)
+    },
+    submit() {
+      console.log("Form is valid". this.v$)
     }
   }
 }
